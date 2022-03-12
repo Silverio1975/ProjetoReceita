f256b56c43a6e1776e1decdb841d42a1c7674cea
Márcio Silvério <ma_silve@yahoo.com.br>
2022-03-11 23:17:38 -0300

    Criado a frase Ola Mundo

--- ProjetoReceita/urls.py
+++ ProjetoReceita/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse  # acrescentado
 from django.urls import path
+
+
+def my_view(request):  #acrescentado
+    return HttpResponse('Olá Mundo!!!')  #acrescentado
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', my_view),  #acrescentado
 ]
